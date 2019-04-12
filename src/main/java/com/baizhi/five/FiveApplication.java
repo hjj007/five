9dd58a991a5477510c526e354f2bd72d4d01e063
hjj007 <49516835+hjj007@users.noreply.github.com>
2019-04-12 13:04:06 +0800

    Update FiveApplication.java

--- src/main/java/com/baizhi/five/FiveApplication.java
+++ src/main/java/com/baizhi/five/FiveApplication.java
@@ -6,6 +6,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FiveApplication {
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     public static void main(String[] args) {
         SpringApplication.run(FiveApplication.class, args);
     }
